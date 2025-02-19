e5e35b1b432f950627d50a61a3134bdb6f23d032
Ghost <dylanbender3@gmail.com>
2025-02-19 12:06:56 -0500

    updated API use for recipes

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './user-routes.js';
+import { nutritionRouter } from './nutrition_api.js';
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/nutrition', nutritionRouter);
 
 export default router;
