573787b8d31851c337558dc3a15a11fe13d1ef4f
Ghost <dylanbender3@gmail.com>
2025-02-21 16:15:18 -0500

    revied header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header>
+            <h1>Gains & Grains</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Workout Plans</a></li>
+                    <li><a href="Nutrition.html">Nutritional Plans</a></li>
+                    <li><a href="#">Forum</a></li>
+                    <li><a href="#">Gyms near me</a></li>
+                    <li><a href="#">gym Keys</a></li>
+                    <li><a href="#">UPGRADE TO PRO</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+export default Header
